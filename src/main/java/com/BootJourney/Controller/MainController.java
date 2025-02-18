a71115e2f1cb7ff7b236fef1e31ec1c0956ae703
donghwan <kdhe9797@naver.com>
2025-02-18 18:04:16 +0900

    test4

--- src/main/java/com/BootJourney/Controller/MainController.java
+++ src/main/java/com/BootJourney/Controller/MainController.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 	
-	@GetMapping("home")
-	@ResponseBody
-	public String index(){
-		return "index";
+	@GetMapping("/home")
+	public String home(){
+		return "home";
 	}
 
 }
