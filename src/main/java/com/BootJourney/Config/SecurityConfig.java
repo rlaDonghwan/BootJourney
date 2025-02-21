3bb08199078519d7854e15a48f3a5e6fd272038a
donghwan <kdhe9797@naver.com>
2025-02-21 13:16:18 +0900

    회원 가입 완료

--- src/main/java/com/BootJourney/Config/SecurityConfig.java
+++ src/main/java/com/BootJourney/Config/SecurityConfig.java
@@ -17,9 +17,11 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
-                );
-
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .formLogin((formLogin) ->
+                        formLogin.loginPage("/user/login")
+                                .defaultSuccessUrl("/"));
+        
         return http.build();
     }
 
--- src/main/java/com/BootJourney/Controller/UserController.java
+++ src/main/java/com/BootJourney/Controller/UserController.java
@@ -5,6 +5,8 @@ import com.BootJourney.Service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +20,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/signup")
-    public String signup(UserCreateForm userCreateForm) {
+    public String signup(Model model) {
+        model.addAttribute("userCreateForm", new UserCreateForm());  // ✅ 폼 객체를 모델에 추가
         return "signup_form";
     }
 
@@ -27,14 +30,42 @@ public class UserController {
         if (bindingResult.hasErrors()) {
             return "signup_form";
         }
+
         if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
             bindingResult.rejectValue("password1", "passwordInCorrect", "2개의 비밀번호가 일치하지 않습니다");
             return "signup_form";
         }
 
-        userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
-
+        try{
+            userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+        }catch (DataIntegrityViolationException e){
+            e.printStackTrace();
+            bindingResult.reject("signupFailed","이미 등록된 사용자 입니다.");
+            return "signup_form";
+        }
         return "redirect:/";
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
